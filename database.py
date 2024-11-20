39cd8836842341757dcd9cdb7362170105e0b3c8
melissalehman73 <melissalehman73@gmail.com>
2024-11-20 01:15:06 +0000

    update to code

--- database.py
+++ database.py
@@ -1,71 +1,69 @@
 import sqlite3
 
 
-def create_user(Firstname, Lastname, Month, Day, Year, Age, birthday_database):
-    conn = sqlite3.connect(f'{birthday_database}.db')
+def create_Family(Firstname, Lastname, Month, Day, family_database):
+    conn = sqlite3.connect(f'{family_database}.db')
     c = conn.cursor()
-    c.execute("INSERT INTO birthdays (Firstname, Lastname, Month, Day, Year, Age) VALUES (?, ?, ?, ?, ?, ?)", (Firstname, Lastname, Month, Day, Year, Age))
-    c.execute("INSERT INTO birthdays (Firstname, Lastname, Month, Day, Year, Age) VALUES (?, ?, ?, ?, ?, ?)", ('Melissa','Lehman', 'May', '31', '1973', '51'))
-    c.execute("INSERT INTO birthdays (Firstname, Lastname, Month, Day, Year, Age) VALUES (?, ?, ?, ?, ?, ?)", ('Andrea','Lehman', 'July', '03', '1998', '26'))
+    c.execute("INSERT INTO Family (Firstname, Lastname, Month, Day) VALUES (?, ?, ?, ?)", (Melissa, Lehman, May, 31))
+    c.execute("INSERT INTO Family (Firstname, Lastname, Month, Day) VALUES (?, ?, ?, ?)", (Andrea, Penny, July, 3))
     conn.commit()
     conn.close()
 
-def update_user(Firstname, Lastname, Month, Day, Year, Age, birthday_database):
-    conn = sqlite3.connect(f'{birthday_database}.db')
+def update_Family(Firstname, Lastname, Month, Day, family_database):
+    conn = sqlite3.connect(f'{family_database}.db')
     c = conn.cursor()
-    c.execute("UPDATE birthdays SET age = ? WHERE Firstname = ? AND Lastname = ?", (age, firstname, Lastname))
+    c.execute("UPDATE Family SET Month = ? AND SET Day = ? WHERE Firstname = ? AND WHERE Lastname = ?", (Month, Day, Firstname, Lastname))
     conn.commit()
     conn.close()
 
-def delete_user(Firstname, Lastname, Month, Day, Year, Age, birthday_database):
-    conn = sqlite3.connect(f'{birthday_database}.db')
+def delete_Family(Firstname, Lastname, Month, Day, family_database):
+    conn = sqlite3.connect(f'{family_database}.db')
     c = conn.cursor()
-    c.execute("DELETE FROM birthdays WHERE firstname = ? AND lastname = ?", (Firstname, Lastname))
+    c.execute("DELETE FROM Family WHERE firstname = ? AND WHERE lastname = ?", (Firstname, Lastname, Month, Day))
     conn.commit()
     conn.close()
 
-def get_user(firstname, lastname, birthday_database):
-    conn = sqlite3.connect(f'{birthday_database}.db')
+def get_Family(firstname, lastname, Month, Day, family_database):
+    conn = sqlite3.connect(f'{family_database}.db')
     c = conn.cursor()
-    c.execute("SELECT * FROM birthdays WHERE firstname = ? AND lastname = ?", (Firstname, Lastname))
+    c.execute("SELECT * FROM Family WHERE firstname = ? AND lastname = ?", (Firstname, Lastname, Month, Day))
     user = c.fetchone()
     conn.close()
-    return user
+    return Family 
 
-def get_user_table(birthday_database):
-    conn = sqlite3.connect(f'{birthday_database}.db')
+def get_Family_table(family_database):
+    conn = sqlite3.connect(f'{family_database}.db')
     c = conn.cursor()
-    c.execute("SELECT * FROM birthdays", ())
-    all_users = c.fetchall()
+    c.execute("SELECT * FROM Family", ())
+    all_Family = c.fetchall()
     conn.close()
-    return all_users
+    return all_Family
 
-def delete_users_from_user_table(birthday_database):
-    conn = sqlite3.connect(f'{birthday_database}.db')
+def delete_Family_from_Family_table(family_database):
+    conn = sqlite3.connect(f'{family_database}.db')
     c = conn.cursor()
-    c.execute("DELETE FROM birthdays")
+    c.execute("DELETE FROM Family")
     conn.commit()
     conn.close()
 
-def print_database(birthday_database):
-    conn = sqlite3.connect(f'{birthday_database}.db')
+def print_database(family_database):
+    conn = sqlite3.connect(f'{family_database}.db')
     c = conn.cursor()
-    c.execute("SELECT * FROM birthdays")
+    c.execute("SELECT * FROM Family")
     rows = c.fetchall()
     for row in rows:
         print(row)
-def create_database(birthday_database):
+def create_database(family_database):
     # Create a connection to the database (it will be created if it doesn't exist)
-    conn = sqlite3.connect('{birthday_database}.db')
+    conn = sqlite3.connect('{family_database}.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS table (
+    c.execute('''CREATE TABLE IF NOT EXISTS Family (
     Firstname TEXT(15),
     Lastname TEXT(15),
     Month TEXT(9),
-    Day INTEGER(2),
-    Year INTEGER(4),
-    Age INTEGER(2)
-                )''')
+    Day INTEGER(2)
+    )''')
+
     # Commit the changes
     conn.commit()
 
