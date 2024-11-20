a46355ba4e532cae40e672fddff0392c28ab945d
melissalehman73 <melissalehman73@gmail.com>
2024-11-20 03:01:40 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import database
 
-# Create the database if it doesn't exist database.create_database()
-database.create_database()
+def print_mydatabase():
+
+    # Create a connection to the database (it will be created if it doesn't exist)
+    Connection = sqlite3.connect('family_database.db')
+
+    # Create a cursor object to execute SQL commands
+    cursor = connection.cursor()
+
+    # Create a Table (if it doesn't exist)
+    Cursor.execute ('''CREATE a Table IF NOT EXISTS users (name, month, day)''')
+    
+    # Commit the changes
+    connection.commit()
+
+    # Close the connection
+    connection.close()
+
+    print ("SQLite3 database created successfully!")
