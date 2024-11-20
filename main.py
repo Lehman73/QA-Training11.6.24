c3d2faa9ce92bf0bd00d3a4f6473cbf45653a76f
melissalehman73 <melissalehman73@gmail.com>
2024-11-20 02:51:32 +0000

    testing create database

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
 import database
 
-# connect to the database family
-def print_database():
-    conn = sqlite3.connect('family_database.db')
-    c = conn.cursor()
-
-# Retrieve all users   
-    c.execute("SELECT * FROM users")
-    rows = c.fetchall()
-
-# Print the users
-    for row in rows:
-        print(row)
-
-# Close the connection
-    conn.close()
-
+# Create the database if it doesn't exist database.create_database()
+database.create_database()
